ff0ad11a67c6687babc9726e5f3e98eab6b09f01
olanrewaju salami <82536846+slarry07@users.noreply.github.com>
2025-10-05 12:46:11 +0100

    Create efs-mount.sh

---
+++ efs-mount.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Update the package repository
+sudo yum update -y
+
+# Create /var/www/html directory
+sudo mkdir -p /var/www/html
+
+# Variable for EFS
+EFS_DNS_NAME=  // dnsname 
+
+# EFS Mount to the /var/www/html
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport "$EFS_DNS_NAME":/ /var/www/html
+
+# Install apache2
+sudo yum install git httpd -y
+
+# Install apache and php dependencies for the php web app
+sudo yum install -y \
+php \
+php-cli \
+php-cgi \
+php-curl \
+php-mbstring \
+php-gd \
+php-mysqlnd \
+php-gettext \
+php-json \
+php-xml \
+php-fpm \
+php-intl \
+php-zip \
+php-bcmath \
+php-ctype \
+php-fileinfo \
+php-openssl \
+php-pdo \
+php-soap \
+php-tokenizer
+
+# Install Mysql-Client 
+sudo wget https://dev.mysql.com/get/mysql80-community-release-el9-1.noarch.rpm 
+sudo dnf install mysql80-community-release-el9-1.noarch.rpm -y
+sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2023
+sudo dnf repolist enabled | grep "mysql.*-community.*"
+sudo dnf install -y mysql-community-server 
+
+# Start and enable Apache & Mysql server
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo systemctl start mysqld
+sudo systemctl enable mysqld
+
+# set /var/www/html directory permissions
+sudo usermod -aG apache ec2-user
+sudo chown -R ec2-user:apache /var/www
+sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
+sudo find /var/www -type f -exec sudo chmod 0664 {} \;
+chown apache:apache -R /var/www/html 
+
+# Download Wordpress files and copy to /var/www/html
+wget https://wordpress.org/latest.tar.gz
+tar -xzf latest.tar.gz
+sudo cp -r wordpress/* /var/www/html/
+
+# create the wp-config.php file
+sudo cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
